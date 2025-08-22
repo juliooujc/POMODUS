84cde6d64c8381f33d640b9923a06e4e39ba40f8
Thais Carolina Braga de Magalhães <thaisbraga969@gmail.com>
2025-08-22 17:23:25 -0300

    criado plano de fundo da pagina inicial

---
+++ frontend/pomodus/src/components/BackgroundWithShape.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material';
+
+
+const BackgroundWithShape = () => {
+    return (
+        // quadrado vermelho
+        <Box sx={{
+            backgroundColor: 'primary.main', // vermelho
+            width: '100vw',
+            height: '100vh'
+        }}>
+            {/* Quadrado branco */}
+            <Box
+                sx={{
+                    backgroundColor: 'secondary.main',
+                    width: '40vw',
+                    height: '100vh'
+                }}>
+
+                {/* Elipse branco */}
+                <Box sx={{
+                    backgroundColor: 'secondary.main',
+                    width: '20vw',
+                    height: '100vh',
+                    marginLeft: '30vw', 
+                    borderTopRightRadius: '50%', // tem aqui ajustar a elipse aq
+                    borderBottomRightRadius: '40%'
+                }}>
+
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default BackgroundWithShape
---
+++ frontend/pomodus/src/theme/theme.jsx
@@ -0,0 +1,15 @@
+// theme.jsx
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#AF3F3F', // vermelho
+    },
+    secondary: {
+      main: '#D9D9D9', // branco
+    },
+  },
+});
+
+export default theme;
