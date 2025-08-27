9a9fb7af752ec9e1a90b4516f7f2efc8f0fe8cb2
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2025-08-27 11:30:50 -0400

    Update setup-and-start.bat

--- backend/setup-and-start.bat
+++ backend/setup-and-start.bat
@@ -1,18 +1,19 @@
 @echo off
+
 set VENV_FOLDER=venv
+echo Verificando ambiente virtual...
 
-echo Checando o ambiente virtual...
-
-rem Checa se o ambiente virtual já existe
+rem Checa se o ambiente virtual existe
 IF NOT EXIST "%VENV_FOLDER%" (
-    echo Ambiente virtual nao encontrado. Criando e instalando dependencias...
+    echo Ambiente virtual nao encontrado. Criando...
     python -m venv "%VENV_FOLDER%"
-    call "%VENV_FOLDER%\Scripts\activate"
-    pip install -r requirements.txt
-) ELSE (
-    echo Ambiente virtual encontrado. Ativando...
-    call "%VENV_FOLDER%\Scripts\activate"
 )
+echo Ativando ambiente virtual...
+rem Ativa o ambiente virtual (funciona mesmo se ele ja existia)
+call "%VENV_FOLDER%\Scripts\activate"
+
+echo Instalando/atualizando dependencias...
+pip install -r requirements.txt
 
 echo Iniciando o servidor Flask...
 python run.py
